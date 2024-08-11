f2ddc784908982c514a4494f319dff832250898b
jiiin <jiiin.cho.dev@gmail.com>
2024-08-11 13:39:06 +0200

    init: setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import Footer from "@/app/_components/footer";
-import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
@@ -10,11 +9,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
-  openGraph: {
-    images: [HOME_OG_IMAGE_URL],
-  },
+  title: `Platter - Food videos and recipes`,
+  description: `The official home of all healthy recipes. Search, watch, and cook every single recipe and video ever - all in one place!`,
 };
 
 export default function RootLayout({
