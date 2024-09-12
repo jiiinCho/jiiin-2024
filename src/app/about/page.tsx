e187a5ed4fc66ffa2f6c5784faa82dc0ee508588
jiiin✨ <jiiin.cho.dev@gmail.com>
2024-09-12 08:14:44 +0200

    style: reponsiveness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,24 @@
 import { Container } from "../_components/container";
+import Footer from "../_components/footer";
 
 export default function About() {
   return (
-    <main>
-      <Container>
-        <p className="text-3xl max-w-[960px] lg:text-5xl leading-snug">
-          👋 Hey there! <br />
-          <br />
-          I'm a frontend developer and UX/UI designer with a passion for
-          crafting intuitive, accessible, and user-friendly experiences using
-          React, Next.js, and TypeScript. <br />
-          <br />
-          Whether it's coding a sleek interface or designing delightful
-          experiences, I'm always up for the next adventure 🧭 🦋
-        </p>
-      </Container>
-    </main>
+    <>
+      <main className="snap-start">
+        <Container>
+          <p className="text-3xl max-w-[960px] lg:text-5xl leading-snug">
+            👋 Hey there! <br />
+            <br />
+            I'm a frontend developer and UX/UI designer with a passion for
+            crafting intuitive, accessible, and user-friendly experiences using
+            React, Next.js, and TypeScript. <br />
+            <br />
+            Whether it's coding a sleek interface or designing delightful
+            experiences, I'm always up for the next adventure 🧭 🦋
+          </p>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
